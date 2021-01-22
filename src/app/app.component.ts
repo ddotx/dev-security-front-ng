35083ccf03b6d8fa13f655d2a86ec4b8ac29ab84
ddotx <domemacintosh@gmail.com>
2021-01-22 14:26:08 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'security-node';
+  // readonly API_URL = 'http://localhost:3000'
+  API_URL = 'https://test.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8'
+  @ViewChild('input') inputData: ElementRef
+
+  constructor(private http: HttpClient){}
+
+  submitForm() {
+    const data = this.inputData.nativeElement.value;
+    console.log(data)
+    const dataObj = {
+      value: data
+    }
+    // this.http.post(`${this.API_URL}/form`, data).subscribe()
+    this.http.post(`${this.API_URL}/form`, dataObj).subscribe()
+  }
 }
